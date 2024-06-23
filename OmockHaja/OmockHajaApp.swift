2fa235aebaad74e45a7ebcd3b558a2a96f396c92
rockintuna <rockintuna@naver.com>
2024-06-23 23:41:11 +0900

    Initial Commit

---
+++ OmockHaja/OmockHajaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmockHajaApp.swift
+//  OmockHaja
+//
+//  Created by 이정인 on 2024/06/23.
+//
+
+import SwiftUI
+
+@main
+struct OmockHajaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
