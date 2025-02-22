a5cf962231f73e1e4bbda0820fd74b91ff1405d0
rockintuna <rockintuna@naver.com>
2025-02-22 23:43:56 +0900

    상대방 돌 출력, 게임 결과 출력

---
+++ OmockHaja/WinView.swift
@@ -0,0 +1,15 @@
+//
+//  WaitingView.swift
+//  OmockHaja
+//
+//  Created by 이정인 on 2/21/25.
+//
+import SwiftUI
+
+struct WinView: View {
+    let message: String
+    
+    var body: some View {
+        Text("Win")
+    }
+}
