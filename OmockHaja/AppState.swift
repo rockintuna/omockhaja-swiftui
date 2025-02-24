8a291fe8711b17fecc5e08b55cd45f2a16ca499a
rockintuna <rockintuna@naver.com>
2025-02-24 23:36:12 +0900

    feat: 메인으로 돌아가기 버튼

---
+++ OmockHaja/AppState.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+class AppState: ObservableObject {
+    @Published var currentView: AppView = .content // 기본값을 ContentView로 설정
+
+    enum AppView {
+        case content
+        case game
+    }
+}
